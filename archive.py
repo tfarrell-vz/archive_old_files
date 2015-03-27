a3726a4a60b59741e6f5e90a5263146a62796edd
tfarrell-vz <farrell@abiresearch.com>
2015-03-27 11:11:04 -0400

    script copies all the directories and their files

---
+++ archive.py
@@ -0,0 +1,38 @@
+import datetime
+import os
+import shutil
+import time
+
+
+ARCHIVE_STORE = '/home/farrell/archive'
+cur_dir = os.getcwd()
+old_path, root = os.path.split(cur_dir)
+archive_root = os.path.join(ARCHIVE_STORE, root)
+os.mkdir(archive_root)
+
+print("The (old) path prior to the directory to be archived %s" % old_path)
+print("Current directory: %s" % cur_dir)
+print("Archives are stored in: %s" % ARCHIVE_STORE)
+print("Archive root for the present archival process: %s" % archive_root)
+
+walk = os.walk(cur_dir)
+
+while True:
+    step = walk.__next__()
+    cur_arch_dir = ARCHIVE_STORE + step[0][len(old_path):]
+    print(cur_arch_dir)
+    #print(step[0]) # dirpath
+    for dir in step[1]: # dirnames
+        os.mkdir(os.path.join(cur_arch_dir, dir))
+
+    for _file in step[2]:
+        shutil.copy(os.path.join(step[0], _file), os.path.join(cur_arch_dir, _file))
+
+
+
+
+
+
+
+
+
