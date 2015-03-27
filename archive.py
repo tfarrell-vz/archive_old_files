2faf6d96ed6ce140d731a5ad1e13c2cd5e461525
tfarrell-vz <farrell@abiresearch.com>
2015-03-27 12:34:19 -0400

    Archives files older than one year; however, the entire directory skeleton is archived.

--- archive.py
+++ archive.py
@@ -4,31 +4,39 @@ import shutil
 import time
 
 
-ARCHIVE_STORE = '/home/farrell/archive'
-cur_dir = os.getcwd()
-old_path, root = os.path.split(cur_dir)
-archive_root = os.path.join(ARCHIVE_STORE, root)
-os.mkdir(archive_root)
+def archive_it(archive_time_limit, target):
+    if time.time() - os.path.getmtime(target) < archive_time_limit:
+        return False
+    else:
+        return True
 
-print("The (old) path prior to the directory to be archived %s" % old_path)
-print("Current directory: %s" % cur_dir)
-print("Archives are stored in: %s" % ARCHIVE_STORE)
-print("Archive root for the present archival process: %s" % archive_root)
+def main():
+    ARCHIVE_TIME = 31556952 # seconds = 365.2425 days
+    ARCHIVE_STORE = '/home/farrell/archive'
+    cur_dir = os.getcwd()
+    old_path, root = os.path.split(cur_dir)
+    archive_root = os.path.join(ARCHIVE_STORE, root)
+    os.mkdir(archive_root)
 
-walk = os.walk(cur_dir)
+    print("The (old) path prior to the directory to be archived %s" % old_path)
+    print("Current directory: %s" % cur_dir)
+    print("Archives are stored in: %s" % ARCHIVE_STORE)
+    print("Archive root for the present archival process: %s" % archive_root)
 
-while True:
-    step = walk.__next__()
-    cur_arch_dir = ARCHIVE_STORE + step[0][len(old_path):]
-    print(cur_arch_dir)
-    #print(step[0]) # dirpath
-    for dir in step[1]: # dirnames
-        os.mkdir(os.path.join(cur_arch_dir, dir))
+    walk = os.walk(cur_dir)
 
-    for _file in step[2]:
-        shutil.copy(os.path.join(step[0], _file), os.path.join(cur_arch_dir, _file))
+    for step in walk:
+        cur_arch_dir = ARCHIVE_STORE + step[0][len(old_path):]
 
+        for dir in step[1]: # dirnames
+            os.mkdir(os.path.join(cur_arch_dir, dir))
 
+        for _file in step[2]:
+            if archive_it(ARCHIVE_TIME, os.path.join(step[0],_file)):
+                shutil.copy(os.path.join(step[0], _file), os.path.join(cur_arch_dir, _file))
+
+if __name__ == '__main__':
+    main()
 
 
 
